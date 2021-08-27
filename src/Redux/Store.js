c5f1433428cceb9a7eb0c6c4e8a44fadf8a92fc2
Saad Adeel <saadadeel8@gmail.com>
2021-08-27 03:00:08 -0700

    initial commit

--- src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import { compose, createStore, applyMiddleware } from 'redux'
-
-
-const loggerMiddleware = require('redux-logger');
-const middleware = [loggerMiddleware.createLogger()];
-const store = createStore(compose(applyMiddleware(...middleware)));
-const store = createStore();
-export default store;
