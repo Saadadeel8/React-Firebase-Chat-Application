4cb86d6fabb4dffe2d45d76782b2d11c4e8e2f2e
Saad Adeel <saadadeel8@gmail.com>
2021-08-27 03:03:22 -0700

    introduced auth features

---
+++ src/Components/Services/base.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app"
+import 'firebase/auth'
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  });
+  console.log(process.env.REACT_APP_FIREBASE_KEY)
+  export const auth = firebaseConfig.auth()
+  export default firebaseConfig
